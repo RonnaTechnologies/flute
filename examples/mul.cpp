c3ca5a0027517d49cc2cbf3cd414ae52e4928836
Jeremy Oden <jerem.oden@gmail.com>
2024-04-20 20:41:56 +0200

    [examples] multiplication example.

--- examples/mul.cpp
+++ examples/mul.cpp
@@ -7,9 +7,18 @@ int main()
     using ufixed_t = ufixed<4, 12>;
     using raw_t = ufixed_t::value_type;
 
+    constexpr double pi_double = 3.14;
+
     constexpr raw_t two{2};
-    constexpr ufixed_t pi{3.14};
+    constexpr ufixed_t pi{pi_double};
 
     constexpr auto two_times_pi = two * pi;
     constexpr auto two_times_pi_double = two_times_pi.as<double>();
+
+    constexpr auto eps = epsilon<ufixed_t, double>::value;
+
+    constexpr auto lb = 2 * pi_double - 2 * eps;
+    constexpr auto ub = 2 * pi_double + 2 * eps;
+
+    static_assert(lb < two_times_pi_double && two_times_pi_double < ub);
 }
