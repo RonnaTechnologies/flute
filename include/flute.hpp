b21ece0c4d95c531e87b55e69bf95b2adf708ed6
RonnaTechnologies <contact@ronnatech.com>
2024-06-16 14:09:22 +0200

    [flute] add 'at' overloads

--- include/flute.hpp
+++ include/flute.hpp
@@ -215,8 +215,28 @@ namespace flute
         using array_type = std::array<T, N>;
         using value_type = T;
 
+        template <typename Callable>
+            requires requires(Callable&& f) { f(typename array_type::size_type{ 0 }); }
+        static constexpr lut<T, N> generate(Callable&& f)
+        {
+            return [](Callable&& f)
+            {
+                using size_type = array_type::size_type;
+                array_type output;
+                for (size_type i = 0; i < N; ++i)
+                {
+                    output[i] = f(i);
+                }
+                return lut<T, N>{ output };
+            }(f);
+        }
 
         constexpr T at(std::size_t i) const noexcept
+        {
+            return data[i];
+        }
+
+        constexpr T& at(std::size_t i) noexcept
         {
             return data[i];
         }
@@ -235,22 +255,6 @@ namespace flute
         static constexpr lut<T, N> make(const std::array<T, N>& input)
         {
             return lut<T, N>{ input };
-        }
-
-        template <typename Callable>
-            requires requires(Callable&& f) { f(typename array_type::size_type{ 0 }); }
-        static constexpr lut<T, N> generate(Callable&& f)
-        {
-            return [](Callable&& f)
-            {
-                using size_type = array_type::size_type;
-                array_type output;
-                for (size_type i = 0; i < N; ++i)
-                {
-                    output[i] = f(i);
-                }
-                return lut<T, N>{ output };
-            }(f);
         }
 
     private:
@@ -281,6 +285,11 @@ namespace flute
         constexpr flut() = delete;
 
         constexpr T at(std::size_t i) const noexcept
+        {
+            return data[i];
+        }
+
+        constexpr T& at(std::size_t i) noexcept
         {
             return data[i];
         }
